99af23dc9e8c7f2a66c3b96a192b4ee81087eb20
saurabhahm <138669028+saurabhahm@users.noreply.github.com>
2023-07-10 10:15:36 +0530

    Add files via upload

---
+++ books_to_scrap_with_sql/lambda.py
@@ -0,0 +1,16 @@
+"""l1=[('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+l1.sort(key=lambda l1:l1[1])
+print(l1)"""
+
+
+"""fib= [0, 1]
+list(map(lambda i: fib.append(sum(fib[-2:])),range(2, int(input("enter a number")))))
+print(fib)"""
+
+"""l3=[1, 2, 3, 5, 7, 8, 9, 10]
+a=list(filter(lambda i:  i&1==0,l3))
+print(f"Number of even numbers in the above array:{len(a)}")
+print(f"Number of odd numbers in the above array :{len(l3)-len(a)}")"""
+"""n=int(input("enter a number"))
+l=[]
+print((n*n) % 1000000007)"""
